42f3a073b7a1e153f35d688f963b35abe7ea7779
Martin <martin.dinahet@proton.me>
2025-03-16 17:19:22 +0100

    added example component

---
+++ src/components/main-text.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MainText: React.FC = () => {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { MainText } from "@/components/main-text";
+
 export const Index: React.FC = () => {
   return (
     <>
-      <div className="w-screen min-h-screen flex justify-center items-center">Hello, World!</div>
+      <div className="w-screen min-h-screen flex justify-center items-center">
+        <MainText />
+      </div>
     </>
   );
 };
